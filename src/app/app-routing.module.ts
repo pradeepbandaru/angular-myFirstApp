6f3d1eff5eadb9a16b9560a956641de57c343889
pradeepbandaru <pradeepkumar.plus@gmail.com>
2021-03-31 19:32:40 +0530

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { BindingComponent } from './pages/binding/binding.component';
-import { DemoComponent } from './pages/demo/demo.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { BindingComponent } from "./pages/binding/binding.component";
+import { DemoComponent } from "./pages/demo/demo.component";
 
-const routes: Routes = [{path: 'demo', component: DemoComponent},
-{path: 'binding', component: BindingComponent}];
+const routes: Routes = [
+  { path: "demo", component: DemoComponent },
+  { path: "binding", component: BindingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
